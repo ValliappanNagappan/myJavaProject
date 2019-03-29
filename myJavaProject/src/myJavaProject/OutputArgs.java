69a63cf8512f8b51abe309f82513642bc4b6207d
vnagapp <vnagapp@BF224226.HHSS.LOCAL>
2019-03-29 17:57:47 -0500

    New

---
+++ myJavaProject/src/myJavaProject/OutputArgs.java
@@ -0,0 +1,18 @@
+package myJavaProject;
+
+public class OutputArgs {
+
+	// public static void main(String[] args) {
+	// for (int i = 0; i < args.length; i++) {
+	// System.out.println(args[i]);
+	public static void main(String[] test) {
+		for (int i = 0; i < test.length; i++) {
+			System.out.println(test[i]);
+			/*
+			 * When some arguments are passed from command line, they will be received in
+			 * args 'args' is just a name and it is a standard. But it can be anything.
+			 */
+		}
+	}
+
+}
--- myJavaProject/src/myJavaProject/SetterAndThis.java
+++ myJavaProject/src/myJavaProject/SetterAndThis.java
@@ -1,25 +1,27 @@
 package myJavaProject;
 
 class Frog {
-	String name;
+	private String name;
 	private int age;
 
 	public String setname(String name) {
 		// this refers to the instance variable declared in the class. And doesn't refer
 		// to the local variable.
 		this.name = name;
+		// System.out.println(name);
 		return name;
 	}
 }
 
 public class SetterAndThis {
 
-	public static String main(String[] args) {
+	public static void main(String[] args) {
 
 		Frog sam = new Frog();
 
 		sam.setname("Sam");
-		System.out.println(sam.setname(name));
+		// System.out.println(sam.setname());
+		// sam.setname();
 	}
 
 }
