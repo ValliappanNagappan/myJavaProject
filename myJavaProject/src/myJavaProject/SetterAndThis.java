c3fba736d54f75579477f7c2bfc3d269de2280cd
vnagapp <vnagapp@BF224226.HHSS.LOCAL>
2019-03-31 17:15:52 -0500

    SettersAndThis

--- myJavaProject/src/myJavaProject/SetterAndThis.java
+++ myJavaProject/src/myJavaProject/SetterAndThis.java
@@ -4,13 +4,17 @@ class Frog {
 	private String name;
 	private int age;
 
-	public String setname(String name) {
+	public String setName(String name) {
 		// this refers to the instance variable declared in the class. And doesn't refer
 		// to the local variable.
 		this.name = name;
 		// System.out.println(name);
 		return name;
 	}
+
+	// public String getName() {
+	// return name;
+	// }
 }
 
 public class SetterAndThis {
@@ -18,10 +22,13 @@ public class SetterAndThis {
 	public static void main(String[] args) {
 
 		Frog sam = new Frog();
+		String name1 = "sam";
+		// sam.setName(name1);
 
-		sam.setname("Sam");
-		// System.out.println(sam.setname());
-		// sam.setname();
+		// sam.setName("Sam");
+		System.out.println(sam.setName("Tom"));
+
+		// System.out.println(name1);
 	}
 
 }
